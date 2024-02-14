54618bef19a06eea186f277ec50ff310ee0e206c
SudhakarM <SudhakarM@R360-LT-027>
2024-02-14 16:28:53 +0530

    Assertion basics

---
+++ AutomationTesting/src/Selenium/Locators2.java
@@ -0,0 +1,35 @@
+package Selenium;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class Locators2 {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Automation Testing\\Webdrivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		String name = "Sudhakar";
+		String password = "rahulshettyacademy";
+		String website = "https://rahulshettyacademy.com/locatorspractice/";
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get(website);
+		driver.manage().window().maximize();
+		driver.findElement(By.id("inputUsername")).sendKeys(name);
+		driver.findElement(By.name("inputPassword")).sendKeys(password);
+		driver.findElement(By.className("signInBtn")).click();
+		driver.findElement(By.cssSelector("label[for='chkboxOne']")).click();
+		driver.findElement(By.cssSelector("input[name='chkboxTwo']")).click();
+		Thread.sleep(1000);
+		System.out.println(driver.findElement(By.xpath("//p")).getText());
+		Assert.assertEquals(driver.findElement(By.xpath("//p")).getText(),"You are successfully logged in.");
+		Assert.assertEquals(driver.findElement(By.cssSelector("div[class='login-container'] h2")).getText(),"Hello "+name+",");
+		
+
+	}
+
+}
