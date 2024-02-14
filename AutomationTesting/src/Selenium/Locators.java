abbda033cf40e249f0ddb0166244153635de978e
SudhakarM <SudhakarM@R360-LT-027>
2024-02-14 14:13:07 +0530

    All syntaxes for locators.

--- AutomationTesting/src/Selenium/Locators.java
+++ AutomationTesting/src/Selenium/Locators.java
@@ -8,13 +8,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Locators {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		
-		System.setProperty("webdriver.chrome.driver","C:\\Automation Testing\\Selenium Browser Webdrivers\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","C:\\Automation Testing\\Webdrivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		driver.manage().window().maximize();
 		driver.findElement(By.id("inputUsername")).sendKeys("rahul");
 		driver.findElement(By.name("inputPassword")).sendKeys("hello");
 		driver.findElement(By.className("signInBtn")).click();
@@ -23,17 +24,20 @@ public class Locators {
         driver.findElement(By.cssSelector("input[placeholder='Name']")).sendKeys("Sudhakar");
         driver.findElement(By.xpath("//input[@type='text'][2]")).sendKeys("abc@yopmail.com");
         driver.findElement(By.cssSelector("input[placeholder='Email']")).clear();
-        driver.findElement(By.xpath("//input[@type='text'][2]")).sendKeys("abc123@gmail.com");
-        driver.findElement()
-        
-        	
-        
-        
-        
-		
-		
-		
-
+        driver.findElement(By.xpath("//input[@type='text'][2]")).sendKeys("abc123@gmail.com");		
+        driver.findElement(By.xpath("//input[@type='text'][3]")).sendKeys("79874324749");
+        driver.findElement(By.xpath("//div/button[2]")).click();
+        System.out.println(driver.findElement(By.xpath("//p[@class='infoMsg']")).getText());
+        driver.findElement(By.xpath("//button[contains(@class,'go')]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//input[contains(@type,'text')][1]")).sendKeys("rahul");
+        driver.findElement(By.cssSelector("input[placeholder*='Pass']")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.cssSelector("label[for*='chkboxOne']")).click();
+        driver.findElement(By.id("chkboxTwo")).click();
+        driver.findElement(By.xpath("//button[@class='submit signInBtn']")).click();
+        Thread.sleep(2000);
+        driver.close();
+               
 	}
 
 }
