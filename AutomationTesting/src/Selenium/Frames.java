05cba502031e798898dc21565b2ed4745b079f55
SudhakarM <SudhakarM@R360-LT-027>
2024-03-20 21:33:23 +0530

    update

--- AutomationTesting/src/Selenium/Frames.java
+++ AutomationTesting/src/Selenium/Frames.java
@@ -1,21 +1,35 @@
 package Selenium;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
 public class Frames {
 
 	public static void main(String[] args) {
 		
+		WebDriver driver = new ChromeDriver();
 		
+		driver.get("https://jqueryui.com/droppable/");
 		
+	    System.out.println(driver.findElements(By.tagName("Iframe")).size());
+	    driver.switchTo().frame(0);
+	    
 		
+	//	driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@class='demo-frame']")));
 		
+		driver.findElement(By.id("draggable")).click();
 		
+		Actions a = new Actions(driver);
 		
+		WebElement drag = driver.findElement(By.id("draggable"));
+		WebElement drop = driver.findElement(By.id("droppable"));
 		
+		a.dragAndDrop(drag,drop).build().perform();
 		
-		
-		
-		
-		
+	    driver.switchTo().defaultContent();
 		
 	}
 
